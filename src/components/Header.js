3979dd8faf2460145ce057b6f07804283550ca3c
Donovan Rimer <donnyjrimer@gmail.com>
2024-09-05 20:39:49 -0700

    fixed coloring issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,16 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-import DBLogo from '../images/DailyBruinLogo.svg'
+import DBLogo from '../images/DailyBruinLogo.svg';
 
 const DBHeader = styled("div")`
   z-index: 2001;
-
   position: -webkit-sticky;
   position: sticky;
   top: 0;
   background: black;
   width: 100%;
   padding: 0.2em 0;
-  color: white;
+  color: white; 
   font-family: 'ITC Century';
   font-style: normal;
   font-weight: 400;
@@ -19,18 +18,22 @@ const DBHeader = styled("div")`
   text-transform: uppercase;
   font-size: 18px;
   line-height: 21.6px;
-  /* border-bottom: 2px solid black; */
+
+  /* Override default link styles */
+  a {
+    color: white; /* Set the color for the link text */
+    text-decoration: none; /* Remove underline if desired */
+  }
 `;
 
 const Header = () => {
   return (
     <DBHeader>
-      {/* Daily Bruin */}
       <a href="https://dailybruin.com">
-        <img src={DBLogo} alt="Daily Bruin" />
+        Daily Bruin
       </a>
     </DBHeader>
-  )
-}
+  );
+};
 
 export default Header;

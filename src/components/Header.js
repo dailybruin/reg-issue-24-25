36572f3e21c2efa5e03d2e895d50b0befdb4c6bb
lexwang3 <lwang3@media.ucla.edu>
2024-09-05 21:19:42 -0700

    updatedheaderandfooter (#4)
    * updatedheaderandfooter
    
    * removed unused import

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,15 @@
 import styled from 'styled-components';
 
-import DBLogo from '../images/DailyBruinLogo.svg';
 
 const DBHeader = styled("div")`
   z-index: 2001;
   position: -webkit-sticky;
   position: sticky;
   top: 0;
-  background: black;
+  background: #F6E4E1;
   width: 100%;
   padding: 0.2em 0;
-  color: white; 
+  color: #4C6953; 
   font-family: 'ITC Century';
   font-style: normal;
   font-weight: 400;
@@ -21,7 +20,7 @@ const DBHeader = styled("div")`
 
   /* Override default link styles */
   a {
-    color: white; /* Set the color for the link text */
+    color: #4C6953; /* Set the color for the link text */
     text-decoration: none; /* Remove underline if desired */
   }
 `;

7331b7d6df7d951c82838f515a2c619e7232012d
Donovan Rimer <donnyjrimer@gmail.com>
2024-09-15 23:47:34 -0700

    added background

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import EditorLetter from './components/EditorLetter';
+import styled from 'styled-components';
+import background from './images/background.png';
+import lights from './images/lights.png';
 
 function App() {
   const [ data, setData ] = useState(null);
@@ -13,10 +16,32 @@ function App() {
 		.then(res => setData(res.data['article.aml']))
   }, [])
 
+  const GradientBackground = styled.div`
+  position: relative;
+  width: 100%;
+  height: auto;
+  background: #4C6953;
+  z-index: -1;
+`
+
+const Background = styled.div`
+position: relative;
+width: 100%;
+height: 8295.32px;  /* Change to auto or fit-content if you build cards to expand down */
+background: 
+    url(${background}), /* top layer background */
+    url(${lights}); /* bottom layer background */
+background-repeat: no-repeat;
+`;
   return data && (
     <div className="App">
       <Header/>
       <EditorLetter editor_letter={data.editor_letter}/>
+      <GradientBackground>
+      <Background>
+        {/* content here */}
+      </Background>
+      </GradientBackground>
       <Footer/>
     </div>
   );

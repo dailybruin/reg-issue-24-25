6eb718397f22c58ffadff9795d67fef1ef99948f
tsakshi01 <tsakshi01@gmail.com>
2024-09-18 23:44:08 -0700

    Fixed Landing Animation

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,6 +2,7 @@
 import React from "react";
 import styled from "styled-components";
 import { mediaQueries } from "../shared/config";
+import Animation from "../images/Landing.gif";
 
 const Container = styled.div`
   width: 100%;
@@ -26,7 +27,7 @@ const Landing = ({landing}) => {
   return (
     <>
       <Container>
-        <Image src={landing[0].landing_image} alt="Landing image" />
+        <Image src={Animation} alt="Landing image" />
       </Container>
     </>
   );

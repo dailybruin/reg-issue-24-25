b3517a5627d4306c61497db8b1926e6f265b1a8f
tsakshi01 <54612666+tsakshi011@users.noreply.github.com>
2024-09-19 13:55:07 -0700

    Revert "added interactive files"

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ import styled from 'styled-components';
 import background from './images/background.png';
 import lights from './images/lights.png';
 
-
 function App() {
   const [ data, setData ] = useState(null);
   
@@ -52,7 +51,6 @@ background-repeat: no-repeat;
 `;
   return data && (
     <div className="App">
-      <generateWordSet></generateWordSet>
       <Layout>
       <div id="landing" key="landing"><Landing landing={data.landing}/></div>
         <Issue/>
